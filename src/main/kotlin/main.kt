09a72ac47964cff3bfe84798872577be43b4a460
Ravil Galeyev <dehasi@yandex.ru>
2021-06-24 19:42:35 +0200

    STEP-3.2

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,9 @@
 import kotlinx.browser.document
+import react.dom.div
 import react.dom.h1
+import react.dom.h3
+import react.dom.img
+import react.dom.p
 import react.dom.render
 
 fun main() {
@@ -7,6 +11,37 @@ fun main() {
     render(document.getElementById("root")) {
         h1 {
             +"Hello, React+KotlinJS"
+        }
+        div {
+            h3 {
+                +"Videos to watch"
+            }
+            p {
+                +"John Doe: Building and breaking things"
+            }
+            p {
+                +"Jane Smith: The development process"
+            }
+            p {
+                +"Matt Miller: The Web 7.0"
+            }
+
+            h3 {
+                +"Videos watched"
+            }
+            p {
+                +"Tom Jerry: Mouseless development"
+            }
+        }
+        div {
+            h3 {
+                +"John Doe: Building and breaking things"
+            }
+            img {
+                attrs {
+                    src = "https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder"
+                }
+            }
         }
     }
 }
