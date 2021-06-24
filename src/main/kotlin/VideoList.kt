6ae2c36d208d8e65a021fedbe26af0bed2eadd95
Ravil Galeyev <dehasi@yandex.ru>
2021-06-24 21:40:54 +0200

    STEP-3.9

--- src/main/kotlin/VideoList.kt
+++ src/main/kotlin/VideoList.kt
@@ -1,3 +1,5 @@
+import kotlinx.browser.window
+import kotlinx.html.js.onClickFunction
 import react.RBuilder
 import react.RComponent
 import react.RProps
@@ -15,6 +17,11 @@ class VideoList : RComponent<VideoListProps, RState>() {
         for (video in props.videos) {
             p {
                 key = video.id.toString()
+                attrs {
+                    onClickFunction = {
+                        window.alert("clicked $video")
+                    }
+                }
                 +"${video.speaker}: ${video.title}"
             }
         }
