b9084b7d656530ffaf1572a730ad157dae0f5ba8
Ravil Galeyev <dehasi@yandex.ru>
2021-06-24 21:27:04 +0200

    STEP-3.5

---
+++ src/main/kotlin/App.kt
@@ -0,0 +1,59 @@
+import kotlinx.css.Position
+import kotlinx.css.position
+import kotlinx.css.px
+import kotlinx.css.right
+import kotlinx.css.top
+import react.RBuilder
+import react.RComponent
+import react.RProps
+import react.RState
+import react.dom.div
+import react.dom.h1
+import react.dom.h3
+import react.dom.img
+import react.dom.p
+import styled.css
+import styled.styledDiv
+
+@JsExport
+class App : RComponent<RProps, RState>() {
+    override fun RBuilder.render() {
+        h1 {
+            +"Hello, React+KotlinJS"
+        }
+        div {
+            h3 {
+                +"Videos to watch"
+            }
+            for (video in unwatchedVideos) {
+                p {
+                    +"${video.speaker}: ${video.title}"
+                }
+            }
+
+            h3 {
+                +"Videos watched"
+            }
+            for (video in watchedVideos) {
+                p {
+                    +"${video.speaker}: ${video.title}"
+                }
+            }
+        }
+        styledDiv {
+            css {
+                position = Position.absolute
+                top = 10.px
+                right = 10.px
+            }
+            h3 {
+                +"John Doe: Building and breaking things"
+            }
+            img {
+                attrs {
+                    src = "https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder"
+                }
+            }
+        }
+    }
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,17 +1,5 @@
 import kotlinx.browser.document
-import kotlinx.css.Position
-import kotlinx.css.position
-import kotlinx.css.px
-import kotlinx.css.right
-import kotlinx.css.top
-import react.dom.div
-import react.dom.h1
-import react.dom.h3
-import react.dom.img
-import react.dom.p
 import react.dom.render
-import styled.css
-import styled.styledDiv
 
 external interface Video {
     val id: Int
@@ -38,44 +26,7 @@ val watchedVideos = listOf(
 )
 
 fun main() {
-
     render(document.getElementById("root")) {
-        h1 {
-            +"Hello, React+KotlinJS"
-        }
-        div {
-            h3 {
-                +"Videos to watch"
-            }
-            for (video in unwatchedVideos) {
-                p {
-                    +"${video.speaker}: ${video.title}"
-                }
-            }
-
-            h3 {
-                +"Videos watched"
-            }
-            for (video in watchedVideos) {
-                p {
-                    +"${video.speaker}: ${video.title}"
-                }
-            }
-        }
-        styledDiv {
-            css {
-                position = Position.absolute
-                top = 10.px
-                right = 10.px
-            }
-            h3 {
-                +"John Doe: Building and breaking things"
-            }
-            img {
-                attrs {
-                    src = "https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder"
-                }
-            }
-        }
+        child(App::class) {}
     }
 }
