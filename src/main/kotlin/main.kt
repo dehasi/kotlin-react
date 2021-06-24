45fe3fecc445aaafdd9b60a381fc98306c840a74
Ravil Galeyev <dehasi@yandex.ru>
2021-06-24 19:57:11 +0200

    STEP-3.3

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -6,6 +6,30 @@ import react.dom.img
 import react.dom.p
 import react.dom.render
 
+external interface Video {
+    val id: Int
+    val title: String
+    val speaker: String
+    val url: String
+}
+
+data class KotlinVideo(
+    override val id: Int,
+    override val title: String,
+    override val speaker: String,
+    override val url: String
+) : Video
+
+val unwatchedVideos = listOf(
+    KotlinVideo(1, "Building and breaking things", "John Doe", "https://youtu.be/PsaFVLr8t4E"),
+    KotlinVideo(2, "The development process", "Jane Smith", "https://youtu.be/PsaFVLr8t4E"),
+    KotlinVideo(3, "The Web 7.0", "Matt Miller", "https://youtu.be/PsaFVLr8t4E")
+)
+
+val watchedVideos = listOf(
+    KotlinVideo(4, "Mouseless development", "Tom Jerry", "https://youtu.be/PsaFVLr8t4E")
+)
+
 fun main() {
 
     render(document.getElementById("root")) {
@@ -16,21 +40,19 @@ fun main() {
             h3 {
                 +"Videos to watch"
             }
-            p {
-                +"John Doe: Building and breaking things"
-            }
-            p {
-                +"Jane Smith: The development process"
-            }
-            p {
-                +"Matt Miller: The Web 7.0"
+            for (video in unwatchedVideos) {
+                p {
+                    +"${video.speaker}: ${video.title}"
+                }
             }
 
             h3 {
                 +"Videos watched"
             }
-            p {
-                +"Tom Jerry: Mouseless development"
+            for (video in watchedVideos) {
+                p {
+                    +"${video.speaker}: ${video.title}"
+                }
             }
         }
         div {
