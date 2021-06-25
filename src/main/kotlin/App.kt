22a3941e56d2312bd9c9c7f9564d9371ee4f6d89
Ravil Galeyev <dehasi@yandex.ru>
2021-06-25 22:52:23 +0200

    STEP-8.3

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,3 +1,8 @@
+import kotlinx.browser.window
+import kotlinx.coroutines.async
+import kotlinx.coroutines.await
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 import react.RBuilder
 import react.RComponent
 import react.RProps
@@ -12,6 +17,21 @@ external interface AppState : RState {
     var currentVideo: Video?
     var unwatchedVideos: List<Video>
     var watchedVideos: List<Video>
+}
+
+suspend fun fetchVideo(id: Int): Video {
+    val response = window
+        .fetch("https://my-json-server.typicode.com/kotlin-hands-on/kotlinconf-json/videos/$id")
+        .await()
+        .json()
+        .await()
+    return response as Video
+}
+
+suspend fun fetchVideos(): List<Video> = coroutineScope {
+    (1..25).map { id ->
+        async { fetchVideo(id) }
+    }.awaitAll()
 }
 
 @JsExport
